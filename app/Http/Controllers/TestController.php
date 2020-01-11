9480d44e78329c5b1e12e8d0e08904c5ade4b158
zlingying <877673916@qq.com>
2020-01-11 15:54:45 +0800

    配置自动上线2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -277,7 +277,7 @@ class TestController extends Controller
     public function gitpull()
     {
         $cmd = 'cd /data/wwwroot/default/api && git pull';
-        shell_exec($cmd);
+        shell_exec($cmd);//
     }
 
 }
